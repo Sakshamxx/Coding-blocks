5caad0c7b30a20810b13b5c946dc70d906586ea7
RYUGA <RYUGA@DESKTOP-IUGH0QO>
2025-01-12 11:27:07 +0530

    hehe

--- Lecture12/stringPrac.java
+++ Lecture12/stringPrac.java
@@ -1,11 +1,15 @@
 package Lecture12;
 public class stringPrac {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         String str1= new String("Hello");
         String str2= new String("Hello");
         String str3= "Hello";
         String str4= "Hello";
-        System.out.println(str1==str2);
-        System.out.println(str3==str4);
+        // System.out.println(str1==str2);
+        // System.out.println(str3==str4);
+
+        String str5=str3+"e";
+        System.out.println(str5);
+        System.out.println(str3);
     }
 }
